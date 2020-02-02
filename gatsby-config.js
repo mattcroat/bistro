86562d7e3beee6cdf8844e0c2264c08212c016fd
mattcroat <mattcroat.dev@gmail.com>
2020-02-02 18:44:33 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,11 +43,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
-}
+};
